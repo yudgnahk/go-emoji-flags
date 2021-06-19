1d9869f7f019fef8fa48e9d85a9187e662836d91
Khang Ha <v.khanghld@vinid.net>
2021-06-19 19:39:17 +0700

    Remove format func

--- emoji_flags.go
+++ emoji_flags.go
@@ -1,7 +1,6 @@
 package emojiflags
 
 import (
-	"fmt"
 	"strings"
 )
 
@@ -16,11 +15,11 @@ func GetFlag(countryCode string) string {
 	switch len(countryCode) {
 	case 2:
 		if code, ok := Alpha2CodeMap[countryCode]; ok {
-			return format(string(0x1F1E6+rune(code[0])-'A') + string(0x1F1E6+rune(code[1])-'A'))
+			return string(0x1F1E6+rune(code[0])-'A') + string(0x1F1E6+rune(code[1])-'A')
 		}
 	case 3:
 		if code, ok := Alpha3CodeMap[countryCode]; ok {
-			return format(string(0x1F1E6+rune(code[0])-'A') + string(0x1F1E6+rune(code[1])-'A'))
+			return string(0x1F1E6+rune(code[0])-'A') + string(0x1F1E6+rune(code[1])-'A')
 		}
 	case 6:
 		if code, ok := SpecialCountryMap[countryCode]; ok {
@@ -31,13 +30,4 @@ func GetFlag(countryCode string) string {
 	}
 
 	return ""
-}
-
-// format ...
-// i need to format those standard emojis to fit the visible length of the special ones
-// it needs a space and 4 invisible characters
-// because standard emoji does not have space and its length is 2
-// and special emoji length is 7
-func format(emoji string) string {
-	return fmt.Sprintf("%s \u00AD\u00AD\u00AD\u00AD", emoji)
 }
